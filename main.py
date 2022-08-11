b470d95f47c140ad7c4822f3c0d7c99b18ce5a4e
Rny8 <neongirl3@yahoo.com>
2022-08-10 20:08:39 -0500

    Added multi item support

--- main.py
+++ main.py
@@ -25,21 +25,37 @@ server.starttls()
 server.login(senderEmail, password)
 print("Login worked")
 
-result = requests.get("https://www.newegg.com/gigabyte-geforce-rtx-3080-ti-gv-n308tgaming-oc-12gd/p/N82E16814932436?Item=N82E16814932436&cm_sp=Homepage_SS-_-P4_14-932-436-_-08102022")
-doc = BeautifulSoup(result.text, "html.parser")
-prices = doc.find_all(text="$")
-parent = prices[0].parent
-strong = parent.find("strong")
-
-currentPrice = int(strong.text)
 
 
+
+
+requestS = []
+gpuDic = {
+    "https://www.newegg.com/gigabyte-geforce-rtx-3080-ti-gv-n308tgaming-oc-12gd/p/N82E16814932436?Item=N82E16814932436&cm_sp=Homepage_SS-_-P4_14-932-436-_-08102022": -1,
+    "https://www.newegg.com/core-black-msi-gp-series-gp66-leopard-11uh-032-gaming-entertainment/p/N82E16834155852?Item=N82E16834155852&cm_sp=Homepage_SS-_-P2_34-155-852-_-08102022": -1
+    
+    }
 
 while True:
-    print(f"Price is: ${currentPrice}")
-    if int(strong.text) == currentPrice:
-        print("Price hasn't changed")
-    else:
-        print("Price has changed!")
-        server.sendmail(senderEmail, rec_email, message)
+    for gpu in gpuDic.keys():
+        request = requests.get(gpu)
+        doc = BeautifulSoup(request.text, "html.parser")
+        prices = doc.find_all(text="$")
+        parent = prices[0].parent
+        strong = parent.find("strong")
+        currentPrice = strong.text
+        if "," in currentPrice:
+            currentPrice = currentPrice.replace(",", "")
+            currentPrice = int(currentPrice)
+        else:
+            currentPrice = int(currentPrice)
+        print(f"Price is: ${currentPrice}")
+        if currentPrice == gpuDic[gpu]:
+            print("Price hasn't changed")
+        else:
+            print("Price has changed!")
+            server.sendmail(senderEmail, rec_email, message)
+        gpuDic[gpu] = currentPrice
+        
     time.sleep(250)
+
