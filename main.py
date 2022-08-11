fe96d516727d9f80905df17c5b84e3f070b0324a
Rny8 <neongirl3@yahoo.com>
2022-08-10 19:19:52 -0500

    Works

---
+++ main.py
@@ -0,0 +1,45 @@
+from bs4 import BeautifulSoup
+import requests
+import time
+import smtplib
+import os
+import json
+
+
+if os.path.exists(os.getcwd() + "/key.json"):
+    with open("./key.json") as f:
+        configData = json.load(f)
+else:
+    configTemplate = {"Password": ""}
+
+    with open(os.getcwd() + "/key.json", "w") as f:
+        json.dump(configTemplate, f)
+
+
+senderEmail = "botneticbots.xyz@gmail.com"
+rec_email = "liljedi06@gmail.com"
+password = configData["Password"]
+message = "Test"
+server = smtplib.SMTP('smtp.gmail.com', 587)
+server.starttls()
+server.login(senderEmail, password)
+print("Login worked")
+
+result = requests.get("https://www.newegg.com/gigabyte-geforce-rtx-3080-ti-gv-n308tgaming-oc-12gd/p/N82E16814932436?Item=N82E16814932436&cm_sp=Homepage_SS-_-P4_14-932-436-_-08102022")
+doc = BeautifulSoup(result.text, "html.parser")
+prices = doc.find_all(text="$")
+parent = prices[0].parent
+strong = parent.find("strong")
+
+currentPrice = int(strong.text)
+
+
+
+while True:
+    print(f"Price is: ${currentPrice}")
+    if int(strong.text) == currentPrice:
+        print("Price hasn't changed")
+    else:
+        print("Price has changed!")
+        server.sendmail(senderEmail, rec_email, message)
+    time.sleep(250)
